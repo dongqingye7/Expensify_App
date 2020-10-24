da97e263938e926446f1df6b0357c4abb6ec5602
Dongqing Ye <dongqingye7@gmail.com>
2020-10-24 12:06:52 -0500

    edit app.js

--- src/app.js
+++ src/app.js
@@ -20,20 +20,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({ description: "water bill", amount: 100, createdAt: 20000 })
-);
-store.dispatch(
-  addExpense({ description: "gas bill", amount: 30, createdAt: 90000 })
-);
-store.dispatch(
-  addExpense({ description: "rent", amount: 1030, createdAt: 1880000 })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
